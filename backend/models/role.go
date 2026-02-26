db9f79e6ce7870af810235961cfef4233e0bf9bf
Ryan Grimsley <ryandgrimsley@gmail.com>
2026-02-26 15:17:03 -0600

    Force usernames to be unique and autogenerate random roleid if one isnt provided

--- backend/models/role.go
+++ backend/models/role.go
@@ -3,19 +3,19 @@ package models
 const (
 	RoleAdmin     = "Admin"
 	RoleDeveloper = "Developer"
-	RoleOwner	  = "Owner"
+	RoleOwner     = "Owner"
 )
 
 type Role struct {
-	RoleID         string   `gorm:"primaryKey;type:varchar(36)" json:"role_id"`
+	RoleID string `gorm:"primaryKey;type:varchar(36);default:gen_random_uuid()" json:"role_id"`
 
 	RoleMerchantID string   `gorm:"not null;type:varchar(36)" json:"role_merchant_id"`
 	Merchant       Merchant `gorm:"foreignKey:RoleMerchantID;references:MerchantID" json:"merchant,omitempty"`
 
-	RoleUserID     string   `gorm:"not null;type:varchar(36)" json:"role_user_id"`
-	User           User     `gorm:"foreignKey:RoleUserID;references:UserID" json:"user,omitempty"`
-	
-	RoleName       string   `gorm:"not null" json:"role_name"`
+	RoleUserID string `gorm:"not null;type:varchar(36)" json:"role_user_id"`
+	User       User   `gorm:"foreignKey:RoleUserID;references:UserID" json:"user,omitempty"`
+
+	RoleName string `gorm:"not null" json:"role_name"`
 }
 
 func (Role) TableName() string {
